22040f88d710ce8e57e075d98573f7463fdef36f
Danyalss <danyalsoltani87@gmail.com>
2024-01-04 20:15:02 +0330

    add

--- download.php
+++ download.php
@@ -4,11 +4,11 @@ $expiry = $_POST['expiry'];
 
 // دانلود ویدیو
 $file = file_get_contents($url);
-$file_path = '/path/to/download/directory/video.mp4';
+$file_path = '/download/video.mp4';
 file_put_contents($file_path, $file);
 
 // ایجاد لینک دانلود
-$download_link = 'http://yourserver.com/path/to/download/directory/video.mp4';
+$download_link = '/download/video.mp4';
 
 // تنظیم زمان انقضا
 $expiry_time = time() + ($expiry * 60 * 60); // convert hours to seconds
